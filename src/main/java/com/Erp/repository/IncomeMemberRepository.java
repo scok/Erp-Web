614f8b3ef01a4bf329ac98eafea821b2dc57b90c
scok <wo7474@naver.com>
2023-06-08 12:33:52 +0900

    손익계산서 - 멤버 통합

---
+++ src/main/java/com/Erp/repository/IncomeMemberRepository.java
@@ -0,0 +1,7 @@
+package com.Erp.repository;
+
+import com.Erp.entity.IncomeMember;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IncomeMemberRepository extends JpaRepository<IncomeMember, Long> {
+}
