c7da87bc2f7cf9393351ed18dfcf392698be98b0
KJH <wo7474@naver.com>
2023-06-09 17:55:26 +0900

    Delete IncomeMemberRepository.java

--- src/main/java/com/Erp/repository/IncomeMemberRepository.java
+++
@@ -1,7 +0,0 @@
-package com.Erp.repository;
-
-import com.Erp.entity.IncomeMember;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IncomeMemberRepository extends JpaRepository<IncomeMember, Long> {
-}
