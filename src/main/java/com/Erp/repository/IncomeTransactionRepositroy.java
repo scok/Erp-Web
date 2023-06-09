81260f80690fd3c15cb2f1f7ddb632eba97b9209
KJH <wo7474@naver.com>
2023-06-09 17:55:33 +0900

    Delete IncomeTransactionRepositroy.java

--- src/main/java/com/Erp/repository/IncomeTransactionRepositroy.java
+++
@@ -1,7 +0,0 @@
-package com.Erp.repository;
-
-import com.Erp.entity.IncomeTransaction;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IncomeTransactionRepositroy extends JpaRepository<IncomeTransaction, Long> {
-}
